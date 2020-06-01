57f281f049b506b82027d725da9df5e60d6dfcd1
Apisapple <38449976+Apisapple@users.noreply.github.com>
2020-06-02 03:16:59 +0900

    20.06.02 문제풀기

--- programmers.co.kr/가운데 글자 가져오기/solution.cpp
+++ programmers.co.kr/가운데 글자 가져오기/solution.cpp
@@ -5,5 +5,20 @@ using namespace std;
 
 string solution(string s) {
     string answer = "";
+    int size = s.size();
+    int index = size / 2;
+    int Odd = size % 2;
+    switch (Odd)
+    {
+    //  짝수
+    case 0:
+        answer.push_back(s.at(index-1));
+        answer.push_back(s.at(index ));   
+        break;
+    // 홀수
+    default:
+        answer.push_back(s.at(index));
+        break;
+    }
     return answer;
 }
