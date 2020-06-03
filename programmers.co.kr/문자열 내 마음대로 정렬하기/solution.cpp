bd3a18862cc2b13b375fd66f421a37024f588609
Apisapple <38449976+Apisapple@users.noreply.github.com>
2020-06-04 03:34:58 +0900

    20.06.03 문제풀이

--- programmers.co.kr/문자열 내 마음대로 정렬하기/solution.cpp
+++ programmers.co.kr/문자열 내 마음대로 정렬하기/solution.cpp
@@ -4,7 +4,19 @@
 
 using namespace std;
 
-vector<string> solution(vector<string> strings, int n) {
+int index = 0;
+
+vector<string> solution(vector<string> strings, int n){
     vector<string> answer;
-    return answer;
-}
+    // 버블정렬
+    for (int i = 0; i < strings.size(); i++){
+        for(int j = i + 1; j <= strings.size() - 1; j++){
+            if(strings[i][n] > strings[j][n]){
+                strings[i].swap(strings[j]);
+            }else if(strings[i][n] == strings[j][n] && strings[i] > strings[j]){
+               strings[i].swap(strings[j]);
+            }
+        }
+    }
+    return strings;
+} 
