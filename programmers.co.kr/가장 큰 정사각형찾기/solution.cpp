80a21b73b2d11a713519f0f9e4c07cb36e37a69a
Apisapple <38449976+Apisapple@users.noreply.github.com>
2020-07-02 00:53:13 +0900

    20.07.01 문제풀이

---
+++ programmers.co.kr/가장 큰 정사각형찾기/solution.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int solution(vector<vector<int>> board)
+{
+    int answer = board[0][0];
+    int height = board.size();
+    int width = board[0].size();
+    for(int i = 1 ; i < height; i ++){
+        for(int j = 1 ; j < width; j ++){
+            if(board[i][j] == 1){
+                board[i][j] == min(board[i][j - 1], board[i - 1][j]);
+                board[i][j] == min(board[i -1][j -1], board[i][j]) + 1;
+                answer = max(answer, board[i][j]);
+            }
+        }
+    }
+    return answer * answer;
+} 
