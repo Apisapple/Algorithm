4963d1880c4c880f7185be3b4a350a13fd1072fc
Apisapple <38449976+Apisapple@users.noreply.github.com>
2020-07-15 17:09:24 +0900

    20.07.15 문제풀이

--- programmers.co.kr/수식 최대화/solution.cpp
+++ programmers.co.kr/수식 최대화/solution.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <string>
 #include <vector>
 #include <iostream>
@@ -5,21 +6,71 @@
 using namespace std;
 
 long long solution(string expression) {
-    vector <int> numbers;
-    vector <char> operaion;
+    vector<long long> numbers;
+    vector<char> operation;
+    vector<char> operationPriority;
+    vector<vector <char>> priority;
+
     string temp;
     long long answer = 0;
     int index = 0;
 
     /* 숫자와 연산자 구분 */
-    for(int i = 0; i < expression.length(); i ++) {
-        if(expression[i] == '+' || expression[i] == '*' || expression[i] == '-') {
-            temp = expression.substr(index, i );
+    for (int i = 0; i < expression.length(); i++) {
+        if (expression[i] == '+' || expression[i] == '*' || expression[i] == '-') {
+            temp = expression.substr(index, i);
             index = i + 1;
-            numbers.push_back(stoi(temp));
-            operaion.push_back(expression[i]);
+            numbers.push_back(stol(temp));
+            operation.push_back(expression[i]);
         }
     }
-    numbers.push_back(stoi(expression.substr(index)));
+    operationPriority.push_back('+');
+    operationPriority.push_back('-');
+    operationPriority.push_back('*');
+    numbers.push_back(stol(expression.substr(index)));
+    sort(operationPriority.begin(), operationPriority.end());
+    
+    /* 연산자 우선 쉰위 순열 구하기 */
+    do {
+        priority.push_back(operationPriority);
+    } while(next_permutation(operationPriority.begin(), operationPriority.end()));
+
+
+    /* 절댓값이 제일 큰 수 찾기 */
+    for(auto operPriority : priority) {
+        vector <long long> copyNumbers = numbers;
+        vector <char> copyOperation = operation;
+        for(char oper : operPriority) {
+            for(int i = 0 ; i < copyOperation.size(); i ++){
+                if(copyOperation[i] == oper) {
+                    switch (oper)
+                    {
+                    case '*':
+                        copyNumbers[i] *= copyNumbers[i + 1];
+                        copyOperation.erase(copyOperation.begin() + i);
+                        copyNumbers.erase(copyNumbers.begin() + i + 1);
+                        i --;
+                        break;
+                    case '+':
+                        copyNumbers[i] += copyNumbers[i + 1];
+                        copyOperation.erase(copyOperation.begin() + i);
+                        copyNumbers.erase(copyNumbers.begin() + i + 1);
+                        i --;
+                        break;
+                    case '-':
+                        copyNumbers[i] -= copyNumbers[i + 1];
+                        copyOperation.erase(copyOperation.begin() + i);
+                        copyNumbers.erase(copyNumbers.begin() + i + 1);
+                        i --;
+                        break;
+                    default:
+                        break;
+                    }
+                }
+            }
+        }
+        if(abs(copyNumbers[0]) > answer)
+            answer = abs(copyNumbers[0]);
+    }
     return answer;
 }
