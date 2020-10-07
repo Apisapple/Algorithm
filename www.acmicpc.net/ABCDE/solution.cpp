43569c3c0cbdd4036aed0cd83d6d5b2c15ac6613
Apisapple <38449976+Apisapple@users.noreply.github.com>
2020-10-07 23:08:48 +0900

    문제풀이

--- www.acmicpc.net/ABCDE/solution.cpp
+++ www.acmicpc.net/ABCDE/solution.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
@@ -7,24 +8,45 @@ using namespace std;
 
 // 전역변수
 int people_cnt, relation_cnt;
-bool flag = false;
+int flag = 0;
+vector <vector <int> > relation;
+vector <bool> visited;;
+
+void DFS(int dep, int index) {
+    // 4번 이어지면 성공
+    visited[index] = true;
+    if(dep == 4) {
+        flag = 1;
+        return;
+    }
+    //  완전 탐색
+    for(int i = 0; i <  relation[index].size(); i++) {
+        int next = relation[index][i];
+        if(!visited[next]) {
+            DFS(dep + 1, next);
+        }
+    }
+    visited[index] = false;
+    return;
+}
 
 int main() {
-
-    // 초기화
-    cin >> people_cnt >> relation_cnt;
-    vector <bool> visited(people_cnt, false);
-    vector <vector <int> > relation(people_cnt + 1, vector <int> (people_cnt + 1, 0));
-
+    
     // 관계 입력
-    for(int i = 0; i < relation_cnt; i++) {
-        int start, end;
-        cin >> start >> end;
-        relation[start][end] = 1;
-        relation[end][start] = 1;
+    cin >> people_cnt >> relation_cnt;
+    visited.resize(people_cnt, false);
+    relation.resize(people_cnt);
+    for(int i = 0 ; i < relation_cnt; i++) {
+        int first, second;
+        cin >> first >> second;
+        relation[first].push_back(second);
+        relation[second].push_back(first);
     }
-
-
-
+    for(int i = 0; i < relation.size(); i++) {
+        DFS(0, i);
+        fill(visited.begin(), visited.end(), false);
+    }
+  
+    cout << flag << endl;
     return 0;
 }
