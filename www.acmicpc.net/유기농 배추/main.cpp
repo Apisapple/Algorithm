906891f8f83d7edad283067e35516e822b551f9b
Apisapple <38449976+Apisapple@users.noreply.github.com>
2020-12-01 01:33:04 +0900

    Update main.cpp
    유기농 배추 풀이

--- www.acmicpc.net/유기농 배추/main.cpp
+++ www.acmicpc.net/유기농 배추/main.cpp
@@ -1,52 +1,54 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
-#define MAX 51
+#define MAX_SIZE 51
 
 using namespace std;
 
-int dx[4] = {0, 0, -1, 1};
-int dy[4] = {1, -1, 0, 0};
-int width, height;
-void DFS(vector< vector<int> > map, vector<vector<bool> > &visit, int x, int y) {
-    visit[x][y] = true;
-    for(int k = 0; k < 4; k ++) {
-        int next_x = x + dx[k];
-        int next_y = y + dy[k];
-        if(0 <= next_x || next_x < width || 0 <= next_y || next_y < height) {
-            if(map[next_x][next_y]  == 1 && !visit[next_x][next_y]) {
-                DFS(map, visit, next_x, next_y);
+int width = 0, height = 0, total_count = 0;
+int countArea = 0;
+int dx[4] = {1, -1, 0, 0};
+int dy[4] = {0, 0, 1, -1};
+
+void DFS(int height_value, int width_value, vector <vector <int>> board, vector< vector<bool>> &visited) {
+    visited[height_value][width_value] = true;
+    for(int i = 0; i < 4; i ++) {
+        int next_x = width_value + dx[i];
+        int next_y = height_value + dy[i];
+        if(0 <= next_x  && next_x < width && 0 <= next_y && next_y < height) {
+            if(board[next_y][next_x] == 1 && !visited[next_y][next_x]) {
+                DFS(next_y, next_x, board, visited);
             }
         }
     }
 }
 
-int main(int argc, const char** argv) {
-    int test_case, point;
-    int result = 0;
+int main() {
+    int test_case = 0;
     cin >> test_case;
-    for(int i = 0; i < test_case; i ++) {
-        cin >> width >> height >> point;
-        vector< vector<int> > map(width, vector<int> (height, 0));
-        vector< vector<bool> > visit(width, vector<bool>(height, false));
-        // 좌표 입력
-        for(int j = 0; j < point; j++) {
-            int x, y;
-            cin >> x >> y;
-            map[x][y] = 1;
+    for(int i = 0; i < test_case; i++) {
+        cin >> width >> height >> total_count;
+        countArea = 0;
+        vector< vector <int> > board(height, vector <int> (width, 0 ));
+        vector< vector <bool> > visited(height, vector <bool> (width, false ));
+        // 배추 심기
+        for(int j = 0; j < total_count; j++) {
+            int width_value, height_value;
+            cin >> width_value >> height_value;
+            board[height_value][width_value] = 1;
         }
 
         // 탐색
-        for(int p = 0; p < width; p++) {
-            for(int q = 0; q < height; q++) {
-                if(map[p][q] == 1 && !visit[p][q]) {
-                    DFS(map, visit, p, q);
-                    result ++;
+        for(int p = 0; p < height; p++) {
+            for(int q = 0; q < width; q++) {
+                if(board[p][q] == 1 && !visited[p][q]) {
+                    countArea ++;
+                    DFS(p, q, board, visited);
                 }
             }
         }
-        cout << result << endl;
+        cout << countArea << endl;
     }
-    
     return 0;
 }
