c1eab3579c5086ab69a4d20d842e090435403887
Apisapple <38449976+Apisapple@users.noreply.github.com>
2020-12-07 02:47:56 +0900

    Create solution.cpp
    최대 힙 문제풀이

---
+++ www.acmicpc.net/최대 힙/solution.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    priority_queue <int, vector<int> > pq;
+    int calCount = 0;
+    // 연산 횟수 입력
+    scanf("%d", &calCount);
+    for(int i = 0; i < calCount; i++) {
+        int number;
+        scanf("%d", &number);
+        switch (number)
+        {
+        case 0:
+            if(pq.empty()) {
+                printf("%d\n", 0);
+            } else {
+                int temp = pq.top();
+                printf("%d\n" , temp);
+                pq.pop();
+            }
+            break;
+        default:
+            pq.push(number);
+            break;
+        }
+    }
+
+    return 0;
+}
