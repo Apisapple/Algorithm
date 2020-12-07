d88b6dc9efeccee3b9fdb0ab84cc187a85c99585
Apisapple <38449976+Apisapple@users.noreply.github.com>
2020-12-08 02:31:57 +0900

    Update solution.cpp
    2 X N 문제풀이

--- www.acmicpc.net/2xN 타일링/solution.cpp
+++ www.acmicpc.net/2xN 타일링/solution.cpp
@@ -4,6 +4,16 @@
 using namespace std;
 
 int main() {
-    
+    int N;
+    cin >> N;
+    vector<int> dp(N + 1);
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 3;
+    for(int i = 4; i <= N; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+    }
+    cout << dp[N] << endl;
     return 0;
 }
