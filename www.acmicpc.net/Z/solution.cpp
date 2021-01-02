c0330a43d8234a1413550e3d21f9c17175ddb948
Apisapple <38449976+Apisapple@users.noreply.github.com>
2021-01-02 22:48:56 +0900

    Update solution.cpp
    Z 문제풀이

--- www.acmicpc.net/Z/solution.cpp
+++ www.acmicpc.net/Z/solution.cpp
@@ -6,45 +6,43 @@
 
 using namespace std;
 
+pair <int, int> mv[4] = { {0, 0}, {0, 1}, {1, 0}, {1, 1}};
 int N = 0, r = 0, c = 0;
 int answer = 0;
 
-void solve(int x, int y, int size) {
-    // N은 자연수이기 때문에 제일 작은 사이즈는 2
-    cout << size << endl;
-    if(size == 2) {
-        if(x == c && y == r) {
+void solve(int x_index, int y_index, int board_size) {
+    if(board_size == 2) {   // 만약 board의 사이즈가 2 (가장 작은 사이즈) 라면..
+        if(r == x_index && c == y_index) {
             cout << answer << endl;
             return;
-        }
-        answer ++;
-        if(x + 1 == c && y == r) {
-            cout  << answer << endl;
+        } else if(r == x_index && c == y_index + 1) {
+            cout << answer + 1 << endl;
+            return;
+        } else if(r == x_index + 1 && c == y_index) {
+            cout << answer + 2<< endl;
+            return;
+        } else if(r == x_index + 1 && c == y_index + 1) {
+            cout << answer + 3 << endl;
             return;
         }
-        answer ++;
-        if(x == c && y + 1 == r) {
-            cout << answer << endl;
-            return;
+        answer += 4;
+    } else { // board의 사이즈가 2가 아니라면...
+        int next_size = (board_size >> 1);
+        for(int i = 0; i < 4; i++) {
+            int nx = x_index + mv[i].first * next_size;
+            int ny = y_index + mv[i].second * next_size;
+            if(nx <= r && r < nx + next_size && ny <= c && c < ny + next_size) {
+                solve(nx, ny, next_size);
+            } else {
+                answer += next_size * next_size;
+            }
         }
-        answer ++;
-        if(x + 1 == c && y + 1 == r) {
-            cout << answer << endl;
-            return;
-        }
-        answer ++;
-        return;
-    }else {
-        int value = size / 2;
-        solve(x, y, value);
-        solve(x + value, y, value);
-        solve(x, y + value, value);
-        solve(x + value, y + value, value);
     }
 }
 
 int main() {
-
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
     cin >> N >> r >> c;
     solve(0, 0, 1 << N);
     return 0;
