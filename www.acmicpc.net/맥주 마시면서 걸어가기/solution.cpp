a4b58dc3c73f1c212a0239e8db2b6589ca203377
Apisapple <38449976+Apisapple@users.noreply.github.com>
2021-01-17 20:52:02 +0900

    Update solution.cpp
    맥주 마시면서 걸어가기 문제풀이

--- www.acmicpc.net/맥주 마시면서 걸어가기/solution.cpp
+++ www.acmicpc.net/맥주 마시면서 걸어가기/solution.cpp
@@ -2,57 +2,61 @@
 #include <iostream>
 #include <vector>
 
-using namespace std;
-
 #define MAX 103
 
-vector<vector<int>> board(MAX, vector<int>(MAX, 0));
-vector<bool> visted(MAX, false);
-int mall_cnt = 0;
+using namespace std;
+int testCase = 0, market_count = 0;
 bool flag = false;
-void DFS(int x, int end) {
-    visted[x] = true;
-    if(x == end)
-        flag = true;
 
-    for(int i = 0; i < mall_cnt + 2; i++) {
-        if(board[x][i] == 1 && !visted[i]) {
-            DFS(i, end);
+void DFS(int start, int end, vector<vector<int>> &board, vector<bool> &visited) {
+    visited[start] = true;
+    if(start == end) {
+        flag = true;
+        return;
+    }
+
+    for(int i = 0; i < market_count + 2; i++) {
+        if(!visited[i] && board[start][i] == 1) {
+            DFS(i, end, board, visited);
         }
     }
 }
 
-int distance(pair<int, int> a, pair<int, int> b) {
-    return abs(a.first - b.first) + abs(a.second - b.second);
-}
-
 int main() {
-    int t = 0;
-    cin >> t;
-    while(t --) {
-        flag = false;
-        cin >> mall_cnt;
-        vector<pair<int, int>> mapping;
+    ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
 
-        for(int i = 0; i < mall_cnt + 2; i++) {
+    cin >> testCase;
+    while(testCase --) {
+        cin >> market_count;
+        vector <vector<int>> board(MAX, vector<int>(MAX, 0));
+        vector <bool> visited(MAX, false);
+        vector <pair<int, int>> vec;
+
+        for(int i = 0; i < market_count + 2; i++) {
             int x, y;
             cin >> x >> y;
-            mapping.push_back(make_pair(x, y));
+            vec.push_back(make_pair(x, y));
         }
-        for(int i = 0 ; i < mapping.size() - 1; i++) {
-            for(int j = 0; j < mapping.size(); j++) {
-                if(distance(mapping[i], mapping[j]) <= 1000) {
+
+        for(int i = 0; i < vec.size() - 1; i++) {
+            for(int j = 0; j < vec.size(); j++) {
+                int distance = abs(vec[i].first - vec[j].first) + abs(vec[i].second - vec[j].second);
+                if(distance <= 1000) {
                     board[i][j] = 1;
                 }
             }
         }
 
-        DFS(0, mall_cnt + 1);
-    
+        DFS(0, market_count + 1, board, visited);
+
         if(flag)
             cout << "happy" << endl;
         else
             cout << "sad" << endl;
+
+        flag = false;
     }
+
     return 0;
 }
