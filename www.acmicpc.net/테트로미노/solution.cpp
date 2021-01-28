e777c3a638c751d865036f7c15f1834ca1ff7cb8
Apisapple <38449976+Apisapple@users.noreply.github.com>
2021-01-28 18:25:10 +0900

    Update solution.cpp
    테트로미노 문제풀이

--- www.acmicpc.net/테트로미노/solution.cpp
+++ www.acmicpc.net/테트로미노/solution.cpp
@@ -4,69 +4,68 @@
 
 using namespace std;
 
+int N, M, ans = 0;
 int dx[4] = {1, -1, 0, 0};
 int dy[4] = {0, 0, 1, -1};
 
-int answer = 0;
-int height = 0;
-int width = 0;
-
-void dfs(int x, int y, vector<vector<int>> board, vector<vector<bool>> &check_visit, int total, int count) {
-    if(count > 3) {
-        answer = max(answer , total);
+void DFS(vector<vector<int>> &board, vector<vector<bool>> &visited, int x, int y, int cnt, int total) {
+    if(cnt == 4) {
+        ans = ans < total ? total : ans;
         return;
-    }
+    } 
     for(int i = 0; i < 4; i ++) {
         int nx = x + dx[i];
         int ny = y + dy[i];
-
-        if(nx < 0 || nx >= height|| ny < 0 || ny >= width)
-            continue;
-        if(!check_visit[nx][ny]) {
-            total += board[nx][ny];
-            cout << "total : " << total << endl;
-            check_visit[nx][ny] = true;
-            dfs(nx, ny, board, check_visit, total, count + 1);
-            check_visit[nx][ny] = false;
+        if(0 <= nx && nx < N && 0 <= ny && ny < M) {
+            if(!visited[nx][ny]) {
+                visited[nx][ny] = true;
+                DFS(board, visited, nx, ny, cnt + 1, total + board[nx][ny]);
+                visited[nx][ny] = false;
+            }
         }
     }
 }
-
-int solution(vector<vector<int>> board, vector<vector<bool>> &check_visit) {
-    for(int i = 0; i < height; i++) {
-        for(int j = 0; j < width; j ++) {
-            int total = 0;
-            total += board[i][j];
-            check_visit[i][j] = true;
-            dfs(i, j, board, check_visit, total, 1);
-            check_visit[i][j] = false;
-        }
+void checkingOH(vector<vector<int>> &board, int x, int y) {
+     int sum = 0;
+    //ㅗ
+    if (x - 1 >= 0 && y + 2 < M) {
+        sum = board[x][y] + board[x][y + 1] + board[x][y + 2] + board[x - 1][y + 1];
+        ans = ans < sum ? sum : ans;
     }
-    return answer;
+    //ㅏ
+    if (x + 2 < N && y + 1 < M) {
+        sum = board[x][y] + board[x + 1][y] + board[x + 2][y] + board[x + 1][y + 1];
+        ans = ans < sum ? sum : ans;
+    }
+    //ㅜ
+    if (x + 1 < N && y + 2 < M) {
+        sum = board[x][y] + board[x][y + 1] + board[x][y + 2] + board[x + 1][y + 1];
+        ans = ans < sum ? sum : ans;
+    }
+    //ㅓ
+    if (x + 2 < N && y - 1 >= 0) {
+        sum = board[x][y] + board[x + 1][y] + board[x + 2][y] + board[x + 1][y - 1];
+        ans = ans < sum ? sum : ans;
+    }
 }
-
 int main() {
-    int result = 0;
-    int total = 0;
-    int temp = 0;
-    cin >> height >> width;
-    
-    vector<vector<int>> board(height, vector<int>(width));
-    vector<vector<bool>> check_visit(height, vector<bool>(width, false));
-    for(int i = 0; i < height; i ++) {
-        for(int j = 0; j < width; j ++) {
-            cin >> temp;
-            board[i][j] = temp;
+    cin >> N >> M;
+    vector<vector<int>> board(N, vector <int>(M, 0));
+    vector<vector<bool>> visited(N, vector<bool>(M, false));
+    for(int i = 0; i < board.size(); i++) {
+        for(int j = 0; j < board[i].size(); j++) {
+            cin >> board[i][j];
         }
     }
-    for(int i = 0; i < height; i ++) {
-        for(int j = 0; j < width; j ++) {
-            cout << board[i][j] << " ";
-        }
-        cout << endl;
-    }
-    result = solution(board, check_visit);
 
-    cout << result << endl;
+    for(int i = 0; i < board.size(); i++) {
+        for(int j = 0; j < board[i].size(); j++) {
+            visited[i][j] = true;
+            DFS(board, visited, i, j, 1, board[i][j]);
+            visited[i][j] = false;
+            checkingOH(board, i, j);
+        }
+    }
+    cout << ans << endl;
     return 0;
 }
