c1c683a62b4ad9724b44498f5abcfcf3ccc8da49
Apisapple <38449976+Apisapple@users.noreply.github.com>
2021-04-14 18:46:01 +0900

    요세푸스 문제 풀이

---
+++ www.acmicpc.net/요세푸스 문제/solution.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    int n, k;
+    queue <int> que;
+    cin >> n >> k;
+
+    for(int i = 0 ; i < n; i++) {
+        que.push(i + 1);
+    }
+
+    cout << "<";
+    while(true) {
+        for(int i = 1; i < k; i++) {
+            que.push(que.front());
+            que.pop();
+        }
+        cout << que.front();
+        que.pop();
+        if(!que.empty()) {
+            cout<<", ";
+        } else {
+            break;
+        }
+    }
+    cout << ">";
+    return 0;
+}
