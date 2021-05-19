d424803122ce50e2ce3d4be5906e8f06a267bf23
Apisapple <38449976+Apisapple@users.noreply.github.com>
2021-05-19 22:52:23 +0900

    update solution.cpp
    순열의 순서 문제풀이

--- www.acmicpc.net/순열의 순서/solution.cpp
+++ www.acmicpc.net/순열의 순서/solution.cpp
@@ -2,31 +2,61 @@
 #include <iostream>
 #include <vector>
 
-#define MAX 20
-
 using namespace std;
 
+#define MAX 20
+
+vector<long long> factorial(MAX + 1, 1);
 int main() {
-    int number_list_size = 0;
-    cin >> number_list_size;
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
 
-    int choice = 0;
+    int number = 0, choice = 0;
+
+    cin >> number;
     cin >> choice;
+    for(int i = 1; i < MAX + 1; i++) {
+        factorial[i] = factorial[i - 1] * i;
+    }
 
-    vector<long long> number_list(MAX + 1, 1);
-    vector<bool> check_list(MAX + 1, false);
-
+    vector<bool> visited(MAX + 1, false);
     if(choice == 1) {
-        long long find;
-        cin >> find;
+        long long temp;
+        cin >> temp;
+        vector<int> result(number, 0);
 
-        for(int i = 0; i < number_list_size; i++) {
-            for(int j = 1; j < number_list_size + 1; j++) {
-
+        for(int i = 0; i < number; i++) {
+            for(int j = 1; j < number + 1; j++) {
+                if(visited[j])
+                    continue;
+                if(factorial[number - i - 1] < temp) {
+                    temp -= factorial[number - i - 1];
+                } else {
+                    result[i] = j;
+                    visited[j] = true;
+                    break;
+                }
             }
         }
-    } else if(choice == 2) {
+        for(int i = 0; i < number; i++) {
+            cout << result[i] << ' ';
+        }
+    } else {
+        long long answer = 0;
+        vector<int> inputData(number);
+        for(int i = 0; i < number; i++) {
+            cin >> inputData[i];
+        }
 
+        for(int i = 0; i < number; i++) {
+            for(int j = 1; j < inputData[i]; j++) {
+                if(!visited[j])
+                    answer += factorial[number - i - 1];
+            }
+            visited[inputData[i]] = true;
+        }
+        cout << answer + 1 << '\n';
     }
+
     return 0;
 }
